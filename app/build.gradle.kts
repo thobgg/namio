3591ce8eb81e355f7320a542ec8392f953bf3c51
thobgg <thomas@bgg-mail.de>
2026-08-30 16:56:05 +0200

    v0.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "de.namio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.15"
+        versionCode = 16
+        versionName = "0.16"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
